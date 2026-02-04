4d905e0646d5f315878ae9f25b3db428a2e76c94
aiznk <nothing@nothing>
2026-02-04 12:56:44 +0900

    add comment #1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,8 +13,9 @@ use windows::{
 
 /**
     2026-02-04
-    音声再生はWindows OneCoreを使う。これはWindowsが用意している機会音声。RustからWindows APIを叩き、textの音声を再生する。
+    音声再生はWindows OneCoreを使う。これはWindowsが用意している機械音声。RustからWindows APIを叩き、textの音声を再生する。
     音声再生は非同期で行われるが、待機しないと再生がすぐ終了し、一聴すると音声が再生されていないように聞こえる。なので音声再生の完了を待機することと、割り込みで音声再生を停止する処理が必要。
+    tokioのoneshot::channelで非同期で再生の完了を通知することで、再生の待機と割り込み時の再生終了を行うことができる。
  */
 async fn do_speak(text: &str) -> Result<()> {
     // 音声合成エンジン作成
