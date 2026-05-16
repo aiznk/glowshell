97f4b4da22419113fcb5f886720db48ed03530c1
aiznk <nothing@nothing>
2026-05-16 15:56:53 +0900

    ok

--- src/main.js
+++ src/main.js
@@ -13,18 +13,134 @@ class MainModel {
   }
 }
 
+class ShellInput extends nue.Input {
+  constructor (model) {
+    super({
+      class: 'shell-input',
+    }, {
+      events: ['keydown'],
+    })
+
+    this.model = model
+  }
+
+  focus () {
+    this.elem.focus()
+  }
+
+  enter () {
+    this.emit('enterShellInput', this.getValue())
+  }
+
+  onKeydown (ev) {
+    switch (ev.code) {
+    case 'NumpadEnter':
+    case 'Enter':
+      this.enter()
+      break
+    }
+  }
+}
+
+class ShellLabel extends nue.Label {
+  constructor (model) {
+    super()
+    this.model = model
+  }
+
+  setCwd (cwd) {
+    this.setText(cwd)
+  }
+}
+
+class ShellSep extends nue.Label {
+  constructor (text) {
+    super()
+    this.setText(text)
+  }
+}
+
+class ShellRow extends nue.Div {
+  constructor (model) {
+    super()
+    this.model = model
+
+    this.label = new ShellLabel(this.model)
+    this.add(this.label)
+
+    this.sep = new ShellSep('$')
+    this.add(this.sep)
+
+    this.input = new ShellInput(this.model)
+    this.add(this.input)
+  }
+
+  focus () {
+    this.input.focus()
+  }
+
+  getLabelCwd () {
+    return this.label.getText()
+  }
+
+  flozen () {
+    let value = this.input.getValue()
+    this.remove(this.input)
+    this.input = new nue.Label()
+    this.input.setText(value)
+    this.add(this.input)
+  }
+
+  setLabelCwd (cwd) {
+    this.label.setCwd(cwd)
+  }
+}
+
+class Shell extends nue.Div {
+  constructor (model) {
+    super()
+    this.model = model
+
+    this.add(new ShellRow(this.model))
+  }
+
+  setFirstLabelCwd (cwd) {
+    this.children[0].setLabelCwd(cwd)
+  }
+
+  receive (key, val) {
+    switch (key) {
+    case 'enterShellInput':
+      this.updateByCmdLine(val)
+      break
+    }
+  }
+
+  updateByCmdLine (cmdLine) {
+    let tailRow = this.children[this.children.length-1]
+    let oldCwd = tailRow.getLabelCwd()
+    let newRow = new ShellRow(this.model)
+
+    tailRow.flozen()
+    newRow.setLabelCwd(oldCwd)
+    this.add(newRow) 
+    newRow.focus()
+  }
+}
+
 class Root extends nue.Root {
   constructor () {
     super()
     this.model = new MainModel()
-    this.setText('Hello')
+    
+    this.shell = new Shell(this.model)
+    this.add(this.shell)
   }
 
   async setup () {
     await listen('done_speak', this.onDoneSpeak.bind(this))
     await this.getCwd()
     await this.listCwdDir()
-    this.setText(this.model.refCwd.value)
   }
 
   async getCwd () {
@@ -34,6 +150,8 @@ class Root extends nue.Root {
       console.error(e)
       return showError(e)
     }
+
+    this.shell.setFirstLabelCwd(this.model.refCwd.value)
   }
 
   async listCwdDir () {
